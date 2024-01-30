12df1ad0208e6eaf28ef15d4a6dfbea93c9588e5
jperlo2 <jperlo@mmelectronica.com>
2024-01-30 17:55:25 -0300

    Agregamos clase 'ApplicationDBContext'

---
+++ ChannelMonitor.Api/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChannelMonitor.Api
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
