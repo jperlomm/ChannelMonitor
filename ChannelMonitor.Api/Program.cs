6fb7d7da04e0b631fab85fd324b0bd3b2c07588b
jperlo2 <jperlo@mmelectronica.com>
2024-01-30 17:11:46 -0300

    Instalamos y configuramos Swagger

--- ChannelMonitor.Api/Program.cs
+++ ChannelMonitor.Api/Program.cs
@@ -1,9 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var allowOrigins = builder.Configuration.GetValue<string>("allowOrigins")!;
+
 // Servicios
+
+builder.Services.AddCors(opciones =>
+{
+    opciones.AddDefaultPolicy(configuracion =>
+    {
+        configuracion.WithOrigins(allowOrigins).AllowAnyHeader().AllowAnyMethod();
+    });
+
+});
+
+builder.Services.AddOutputCache();
+builder.Services.AddSwaggerGen();
 
 // Servicios
 var app = builder.Build();
 // Middleware
+
+app.UseCors();
+app.UseOutputCache();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
